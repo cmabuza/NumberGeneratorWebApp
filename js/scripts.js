2f9b689247ebc5e9566d237d4a1f3630794133c7
cmabuza <cmabuza@student.wethinkcode.co.za>
2021-08-25 18:21:37 +0200

    2 random Dice

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,24 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+// const dieNumber = 1;
+// const randomNum;
+// const diceImage;
+// for(var i = 0; i != 2; i++){
+//     randomNum = Math.floor(Math.random() * 6) + 1;
+//     diceImage = '/assests/images/Dice' + randomNum + '.png';
+//     document.querySelectorAll('img')[i].setAttribute('src', diceImage)
+// }
+
+const dieNumber = 1;
+var randomNum;
+var diceImage;
+
+randomNum = Math.floor(Math.random() * 6) + 1;
+diceImage = '/assests/images/Dice' + randomNum + '.png';
+
+document.querySelectorAll('img')[0].setAttribute('src', diceImage)
+randomNum = Math.floor(Math.random() * 6) + 1;
+diceImage = '/assests/images/Dice' + randomNum + '.png';
+document.querySelectorAll('img')[1].setAttribute('src', diceImage)
+// "C:\Users\Botter\Documents\CodeMentor\Easy - Random Number Generator Web App\NumberGeneratorWebApp\assests\images\Dice3.png"
